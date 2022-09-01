29c70f7ef3d78d66226db7fce35f694a3920215a
saepulohjaenudin <55588372+saepulohjaenudin@users.noreply.github.com>
2022-09-01 22:30:13 +0700

    menyalin react

---
+++ precache-manifest.6140dd8f075324a272b01791cbb370a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e63c9fa46c725993b8aa",
+    "url": "/static/css/main.59d0c7be.chunk.css"
+  },
+  {
+    "revision": "e63c9fa46c725993b8aa",
+    "url": "/static/js/main.e63c9fa4.chunk.js"
+  },
+  {
+    "revision": "14149b2e1b6f7d222800",
+    "url": "/static/js/1.14149b2e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71c2f496380be64444b667660c4c0496",
+    "url": "/index.html"
+  }
+];
